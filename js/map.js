7fffe418a5e38147378f4ab8b26ac252e6c7ffbd
etathiago <etathiago2@gmail.com>
2022-10-24 21:47:18 -0300

    Projeto GAMEOVER Rev.17

--- js/map.js
+++ js/map.js
@@ -1,15 +1,16 @@
 jQuery(function ($) {
     function init_map1() {
-        var mylocation = new google.maps.LatLng (-23.4864716,-47.4495972,15);
-        var mapOpition = {
-            center: mylocation,
+        var myLocation = new google.maps.LatLng (-23.4864716,-47.4495972,15);
+        var mapOption = {
+            center: myLocation,
             zoom: 16
         };
         var marker = new google.maps.Marker({
-            position: mylocation,
+            position: myLocation,
             title: "Localização"
         });
-        var map = new google.maps.Map (document.getElementById("map1"),mapOptions);
+        var map = new google.maps.Map(document.getElementById("map1"),
+            mapOption);
         marker.setMap (map);
     }
     init_map1();
